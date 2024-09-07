eeafb42d01c1e27b86be5c4a4037af9cc494dba9
Abhishek-kumar9696 <123757964+Abhishek-kumar9696@users.noreply.github.com>
2024-09-08 01:53:38 +0530

    Update server.js for deployment

--- server.js
+++ server.js
@@ -1,46 +1,55 @@
-import express  from "express"
-import colors from "colors"
-import dotenv  from "dotenv"
+
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
 import morgan from 'morgan';
 import connectDb from './config/db.js';
 import authRoutes from "./routes/authRoute.js";
-import categoryRoutes from "./routes/categoryRoutes.js"
-import productRoutes from './routes/productRoutes.js'
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
+import path from "path";
+import { fileURLToPath } from 'url';
 
-// configure dotenv
+// Configure dotenv
 dotenv.config();
 
-// database cionfig
+// Database config
 connectDb();
 
-
-// rest object
+// Rest object
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
 app.use(morgan('dev'));
 
+// Routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
-// routes
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/category",categoryRoutes);
-app.use("/api/v1/product",productRoutes);
+// Serve static files from the client/build folder in production
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')));
 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send("API is running successfully");
+    });
+}
 
-//rest api
-app.get('/',(req,res) => {
-    res.send("<h1>welcome to ecommerce</h1>")
-})
+// Port
+const PORT = process.env.PORT || 8080;
 
-// port
-const PORT  = process.env.PORT || 8080;
-
-app.listen(PORT,()=>{
-    console.log(`server running on ${PORT}`.bgCyan.white);
-})
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`.bgCyan.white);
+});
